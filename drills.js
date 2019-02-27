c700fe5b988e65d31bcdc7e06b60bc0ddb65edc2
Publikwerker <jason.michael.hoffman@gmail.com>
2019-02-27 11:45:26 -0800

    added maxSum method

--- drills.js
+++ drills.js
@@ -21,5 +21,16 @@ function noSpace(str){
   return temp;
  }
 
+ function maxSum(arr){
+   let highest = 0;
+   for(let i =0; i < arr.length-1; i++){
+    if (arr[i]+arr[i+1]>highest){ 
+      highest = (arr[i]+arr[i+1])
+    };
+   }
+   return highest;
+ }
+
 // console.log(noSpace('The Wild Things'))
-console.log(filterArray([1,2,3,4,5,6,7,8,9]))
+//console.log(filterArray([1,2,3,4,5,6,7,8,9]))
+console.log(maxSum([4,6,-3,5,-2,1]));
