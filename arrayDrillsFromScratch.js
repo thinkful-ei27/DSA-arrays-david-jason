935a7bd12120122d46a9a2a1a3fec68df3e3be66
Publikwerker <jason.michael.hoffman@gmail.com>
2019-02-27 10:10:26 -0800

    read directions and required memory, then created an instance

--- arrayDrillsFromScratch.js
+++ arrayDrillsFromScratch.js
@@ -1,28 +1,29 @@
-import Memory from './memory.js';
+const Memory = require('./memory.js');
+const memory = new Memory();
 
 class Array {
   constructor(){
-    this.length=0,
-    this._capacity=0,
-    this.ptr=Memory.allocate(this.length)
+    this.length = 0,
+    this._capacity = 0,
+    this.ptr = memory.allocate(this.length)
   }
 
   push(value){
     if (this.length >= this._capacity){
       this._resize((this._capacity + 1)*3);
     }
-    Memory.set(this.ptr + this.length, value);
+    memory.set(this.ptr + this.length, value);
     this.length++;
   }
 
   _resize(size) {
     const oldPtr = this.ptr;
-    this.ptr = Memory.allocate(size)
+    this.ptr = memory.allocate(size)
     if (this.ptr === null) {
       throw new Error('Out of memory');
     }
-    Memory.copy(this.ptr, oldPtr, this.length)
-    Memory.free(oldPtr);
+    memory.copy(this.ptr, oldPtr, this.length)
+    memory.free(oldPtr);
     this._capacity = size;
   }
 
@@ -30,7 +31,7 @@ class Array {
     if(index < 0 || index >= this.length){
       throw new Error('Index Error');
     }
-   return Memory.get(this.ptr + index)
+   return memory.get(this.ptr + index)
   }
 
 
