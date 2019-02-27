cb06f4e28e49995a18db307cbfb1febc48780f96
David <dajohnson1998@gmail.com>
2019-02-27 15:03:18 -0500

    finished mergeArray

--- drills.js
+++ drills.js
@@ -31,6 +31,37 @@ function noSpace(str){
    return highest;
  }
 
+ function mergeArray(arr1, arr2) {
+  let temp = [];
+  let length = 0;
+  if (arr1.length > arr2.length) {
+    length = arr1.length
+  } else if (arr1.length < arr2.length){
+    length = arr2.length;
+  } else {
+    length = arr1.length
+  }
+
+  for(let i =0; i < length; i++){
+    if (arr1[i] > arr2[i]) {
+      temp.push(arr2[i])
+      temp.push(arr1[i])
+    } else if (arr1[i] < arr2[i]) {
+      temp.push(arr1[i])
+      temp.push(arr2[i])
+    } else if(arr1[i] === arr2[i]) {
+      temp.push(arr1[i])
+      temp.push(arr2[i])
+    } else if (arr1[i] === undefined) {
+      temp.splice(temp.length - 1, 0, arr2[i] )
+    } else if (arr2[i] === undefined) {
+      temp.splice(temp.length - 1, 0, arr1[i] )
+    }
+  }
+  return temp;
+}
+
 // console.log(noSpace('The Wild Things'))
 //console.log(filterArray([1,2,3,4,5,6,7,8,9]))
-console.log(maxSum([4,6,-3,5,-2,1]));
+// console.log(maxSum([4,6,-3,5,-2,1]));
+console.log(mergeArray([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]))
