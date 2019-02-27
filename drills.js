ea330d9fed433ac7b478effcc60a4e3d6132fc04
David <dajohnson1998@gmail.com>
2019-02-27 14:38:51 -0500

    Finsihed filterArray drill

--- drills.js
+++ drills.js
@@ -11,4 +11,15 @@ function noSpace(str){
  }
  return temp;}
 
-console.log(noSpace('The Wild Things'));
+ function filterArray(arr) {
+  let temp = []
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] > 5) {
+      temp.push(arr[i])
+    }
+  }
+  return temp;
+ }
+
+// console.log(noSpace('The Wild Things'))
+console.log(filterArray([1,2,3,4,5,6,7,8,9]))
